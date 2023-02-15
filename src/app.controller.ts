86164cae9d5342113f706d6f3f84cc562d75ab26
cwshkut <alexey.shkut@clockwise.software>
2023-02-15 15:18:40 +0000

    fix root path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+const pkg = readFileSync(resolve(process.cwd(), 'package.json'));
+const { version, name } = JSON.parse(pkg.toString());
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('')
+  getInfo() {
+    return { version, name };
   }
 }
